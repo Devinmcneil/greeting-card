44200e20fcfa57f48a648d95f10e979c31307c14
devither <Devither421@ed.amdsb.ca>
2017-09-25 12:55:47 -0400

    #2

--- greeting card/Form1.Designer.cs
+++ greeting card/Form1.Designer.cs
@@ -28,7 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.horoscope = new System.Windows.Forms.Label();
+            this.nameTag = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // horoscope
+            // 
+            this.horoscope.AutoSize = true;
+            this.horoscope.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.horoscope.Location = new System.Drawing.Point(270, 9);
+            this.horoscope.Name = "horoscope";
+            this.horoscope.Size = new System.Drawing.Size(183, 30);
+            this.horoscope.TabIndex = 0;
+            this.horoscope.Text = "Pisces Horoscope";
+            // 
+            // nameTag
+            // 
+            this.nameTag.AutoSize = true;
+            this.nameTag.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold);
+            this.nameTag.Location = new System.Drawing.Point(155, 446);
+            this.nameTag.Name = "nameTag";
+            this.nameTag.Size = new System.Drawing.Size(141, 30);
+            this.nameTag.TabIndex = 1;
+            this.nameTag.Text = "Devin Mcneil";
             // 
             // Form1
             // 
@@ -37,15 +59,21 @@
             this.BackgroundImage = global::greeting_card.Properties.Resources.pisces_png;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(465, 485);
+            this.Controls.Add(this.nameTag);
+            this.Controls.Add(this.horoscope);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Click += new System.EventHandler(this.Form1_Click);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label horoscope;
+        private System.Windows.Forms.Label nameTag;
     }
 }
 
--- greeting card/Form1.cs
+++ greeting card/Form1.cs
@@ -24,19 +24,54 @@ namespace greeting_card
         private void Form1_Click(object sender, EventArgs e)
         {
             Graphics formGraphics = this.CreateGraphics();
-            Pen drawPen = new Pen(Color.Red, 10);
-            SolidBrush drawBrush = new SolidBrush(Color.SeaGreen);
+            Pen drawPen = new Pen(Color.Black, 10);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
             Graphics fg = this.CreateGraphics();
-
-            Font drawFont = new Font("Georgia", 20, FontStyle.Bold);
-            fg.DrawString("Devin McNeil", drawFont, drawBrush, 20, 200);
-            fg.DrawString("Pieces greeting card", drawFont, drawBrush, 10, 10);
-            Thread.Sleep(3000);
+            horoscope.Dispose();
+            nameTag.Dispose();
             BackgroundImage = null;
+            Refresh();
 
-            fg.DrawString("The pisces horoscope in from Febuary 19 - March 20.", drawFont, drawBrush, 140, 160);
+            this.BackColor = Color.Red;
+            Font drawFont = new Font("Georgia", 10, FontStyle.Bold);
+            Font = new Font("Georgia", 10, FontStyle.Bold);
+            fg.DrawString("The pisces horoscope lasts from Febuary 19 - March 20.", drawFont, drawBrush, 5,400);
 
+            formGraphics.DrawEllipse(drawPen, 150, 250, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 150, 250, 10, 10);
 
+            formGraphics.DrawEllipse(drawPen, 190, 300, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 190, 300, 10, 10);
+
+            formGraphics.DrawEllipse(drawPen, 230, 250, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 230, 250, 10, 10);
+
+            formGraphics.DrawEllipse(drawPen, 300, 250, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 300, 250, 10, 10);
+
+            formGraphics.DrawEllipse(drawPen, 350, 250, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 350, 250, 10, 10);
+
+            formGraphics.DrawEllipse(drawPen, 400, 280, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 400, 280, 10, 10);
+
+            formGraphics.DrawEllipse(drawPen, 370, 200, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 370, 200, 10, 10);
+
+            formGraphics.DrawEllipse(drawPen, 340, 180, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 340, 180, 10, 10);
+
+            formGraphics.DrawEllipse(drawPen, 300, 100, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 300, 100, 10, 10);
+
+            formGraphics.DrawEllipse(drawPen, 280, 80, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 280, 80, 10, 10);
+
+            formGraphics.DrawEllipse(drawPen, 295, 120, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 295, 120, 10, 10);
+
+            formGraphics.DrawEllipse(drawPen, 295, 120, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 295, 120, 10, 10);
         }
     }
 }
